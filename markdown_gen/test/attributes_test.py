3055fa16010a1b855142c2e5b866d76daee17c8f
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-03-17 19:39:45 +0100

    Add test for bold and italic text

--- markdown_gen/test/attributes_test.py
+++ markdown_gen/test/attributes_test.py
@@ -21,6 +21,14 @@ class AttributesTests(unittest.TestCase):
 
         expected = "__bold text alternative__"
         self.assertEqual(expected, md.gen_bold("bold text alternative", True))
+        
+    def test_bold_and_italic(self):
+        expected = "***bold and italic text***"
+        self.assertEqual(expected, md.gen_italic(md.gen_bold("bold text")))
+        self.assertEqual(expected, md.gen_bold(md.gen_italic("bold text")))
+
+        expected = "__bold text alternative__"
+        self.assertEqual(expected, md.gen_bold("bold text alternative", True))
 
     def test_monspace(self):
         expected = "`monospace`"
