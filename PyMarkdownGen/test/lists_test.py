48240529c9c5c4bbeb961eb32b7f9870aaa951e1
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-07 22:29:45 +0200

    Add docstrings for unit tests for list generation

--- PyMarkdownGen/test/lists_test.py
+++ PyMarkdownGen/test/lists_test.py
@@ -1,10 +1,14 @@
-
+"""This module contains the unit tests for list generation."""
 import unittest
 import PyMarkdownGen.PyMarkdownGen as md
 
 
 class ListTest(unittest.TestCase):
+    """The test case (fixture) for the tests for list generation."""
+
     def test_ordered_list(self):
+        """Create a ordered (numbered) list."""
+
         expected = \
 """1. first
 2. second
@@ -15,13 +19,15 @@ class ListTest(unittest.TestCase):
 
 
     def test_un_ordered_list(self):
+        """Create a unordered bullet list."""
         expected = \
 """* one intem
 * next item
 * one more
 * and so on
 """
-        self.assertEqual(expected, md.gen_un_ordered_list(["one intem", "next item", "one more", "and so on"]))
+        self.assertEqual(expected, md.gen_un_ordered_list(
+            ["one intem", "next item", "one more", "and so on"]))
 
 if __name__ == '__main__':
     unittest.main()
