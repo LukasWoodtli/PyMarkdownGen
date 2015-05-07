d15fbed45ed72f862ebf7debd76684e68babdebd
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-07 22:25:46 +0200

    Add docstings to link tests

--- PyMarkdownGen/test/link_test.py
+++ PyMarkdownGen/test/link_test.py
@@ -1,3 +1,5 @@
+"""This module contains the unit tests for link generation."""
+
 __author__ = 'Boot'
 
 import unittest
@@ -5,15 +7,27 @@ import PyMarkdownGen.PyMarkdownGen as md
 
 
 class LinkTests(unittest.TestCase):
+    """The test case (fixture for link tests."""
+
+
     def test_link(self):
+        """Simple link generation where only the URL is provided."""
+
         expected = "www.example.com"
         self.assertEqual(expected, md.gen_link("www.example.com"))
 
+
     def test_link_with_text(self):
+        """A link that displays a different text than the URL."""
+
         expected = "[This is a link.](www.example.com)"
         self.assertEqual(expected, md.gen_link("www.example.com", "This is a link."))
 
+
     def test_link_with_txt_and_alt_txt(self):
+        """A link that displays a different text than the URL.
+        And has an alternative text."""
+
         expected = '[This is a link.](www.example.com "Alternative text")'
         self.assertEqual(expected,
                          md.gen_link("www.example.com",
@@ -21,6 +35,8 @@ class LinkTests(unittest.TestCase):
                                      "Alternative text"))
 
     def test_image_link(self):
+        """Test a link to an inmage."""
+
         expected = '![alt text](http://example.com/example.jpg "Text")'
         self.assertEqual(expected,
                          md.gen_image_link("http://example.com/example.jpg",
