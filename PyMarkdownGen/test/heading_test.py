e9bda265dd6f4ed9050f8c8f6e1ea6fc48894562
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-07 22:20:53 +0200

    Add docstrings to heading tests

--- PyMarkdownGen/test/heading_test.py
+++ PyMarkdownGen/test/heading_test.py
@@ -1,35 +1,49 @@
-
+"""Tis module contains the unit tests for generating headings."""
 
 import unittest
 
 import PyMarkdownGen.PyMarkdownGen as md
 
 class HeadingTests(unittest.TestCase):
-
+    """The test case (fixture) for generating headings."""
 
     def test_heading_1(self):
+        """Test heading of depth 1. The default variant (with '#')."""
+
         expected = "# Heading 1\n"
         self.assertEqual(expected, md.gen_heading("Heading 1", 1))
 
+
     def test_heading_2(self):
+        """Test heading of depth 2. The default variant (with '##')."""
+
         expected = "## Heading 2\n"
         self.assertEqual(expected, md.gen_heading("Heading 2", 2))
 
+
     def test_heading_4(self):
+        """Test heading of depth 4. The default variant (with '####')."""
         expected = "#### Heading 3\n"
         self.assertEqual(expected, md.gen_heading("Heading 3", 4))
 
+
     def test_heading_1_alternative(self):
+        """Test heading of depth 1. The default alternative variant (with '=')"""
+
         expected = "Heading 1\n" + \
                    "=========\n"
         self.assertEqual(expected, md.gen_heading("Heading 1", 1, True))
 
+
     def test_heading_2_alternative(self):
+        """Test heading of depth 2. The alternative variant (with '-')"""
         expected = "Heading 2\n" + \
                    "---------\n"
         self.assertEqual(expected, md.gen_heading("Heading 2", 2, True))
 
+
     def test_heading_alternative(self):
+        """Test heading of depth 3. The default variant (with '#') is forced."""
         expected = "### Heading 3\n"
         self.assertEqual(expected, md.gen_heading("Heading 3", 3, True))
 
