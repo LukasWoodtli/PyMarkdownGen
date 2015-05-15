6ff6ec26b7442beae87b9037b3dcce5c96408fc8
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-15 10:21:00 +0200

    Add content to setup.py for pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PyMarkdownGen',
+      version='0.1',
+      description='Library to generate markdown formatted text',
+      url='https://github.com/LukasWoodtli/PyMarkdownGen',
+      author='Lukas Woodtli',
+      author_email='woodtli.lukas@gmail.com',
+      license='Eclipse Public License - v 1.0',
+      packages=['PyMarkdownGen'],
+      zip_safe=False)
