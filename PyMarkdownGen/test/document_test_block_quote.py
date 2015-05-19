27c5215c5d2e024dae17a03b9e1d1bcf4bb076e0
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-19 23:08:45 +0200

    Add some tests for block quotes

--- PyMarkdownGen/test/document_test_block_quote.py
+++ PyMarkdownGen/test/document_test_block_quote.py
@@ -6,20 +6,18 @@ __author__ = 'Lukas Woodtli'
 
 import unittest
 from PyMarkdownGen.test.document_test_common import get_expected_markdown_text
+import PyMarkdownGen.PyMarkdownGen as md
 import PyMarkdownGen.Document as doc
 
 class BlockQuoteTest(unittest.TestCase):
     """Test for the original file with block quote."""
 
-    def setUp(self):
-        """Set up the file with the expected output."""
-
-        self.expected_output_text = get_expected_markdown_text("Blockquotes with code blocks.text")
-
-
 
     def test_something(self):
-        """Define a text and create a block qote from it."""
+        """Define a text and create a block quote from it."""
+
+        expected_output_text = get_expected_markdown_text("Blockquotes with code blocks.text")
+
 
         text = \
 """Example:
@@ -37,7 +35,25 @@ Or:
         md_document = doc.Document()
         md_document.add_block_quote(text)
 
-        self.assertEqual(self.expected_output_text, md_document.get_markdown_text())
+        self.assertEqual(expected_output_text, md_document.get_markdown_text())
+
+
+    def test_nested_block_quotes(self):
+        """Test for nested block quotes."""
+
+        expected_output_text = get_expected_markdown_text("Nested blockquotes.text")
+
+        text = "foo"
+        text += md.gen_section()
+        text += md.gen_block_quote("bar")
+        text += "\n"
+        text += "foo"
+
+        md_doc = doc.Document()
+        md_doc.add_block_quote(text)
+
+        self.assertEqual(expected_output_text, md_doc.get_markdown_text())
+
 
 
 if __name__ == '__main__':
