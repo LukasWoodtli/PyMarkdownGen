5e309e96d78f003bab888ed7a3c6f62ad57f4a95
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-20 01:04:55 +0200

    Coverage for testing: add # pragma: no cover for main

--- PyMarkdownGen/test/document_test_block_quote.py
+++ PyMarkdownGen/test/document_test_block_quote.py
@@ -71,4 +71,4 @@ Or:
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() # pragma: no cover
--- PyMarkdownGen/test/document_test_links_references.py
+++ PyMarkdownGen/test/document_test_links_references.py
@@ -96,4 +96,4 @@ class LinkAndReferencesTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() # pragma: no cover
