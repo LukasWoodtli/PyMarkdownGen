df7a51b45c175b7178ad51ff9c854daf91acbf8c
Lukas Woodtli <woodtli.lukas@gmail.com>
2015-05-20 19:16:36 +0200

    Add tests for bolt and italic text

--- PyMarkdownGen/test/document_test_misc.py
+++ PyMarkdownGen/test/document_test_misc.py
@@ -4,6 +4,7 @@ __author__ = 'Lukas Woodtli'
 import unittest
 from PyMarkdownGen.test.document_test_common import get_expected_markdown_text
 import PyMarkdownGen.Document as doc
+import PyMarkdownGen.PyMarkdownGen as md
 
 class TestMisc(unittest.TestCase):
     """Miscellaneous tests."""
@@ -30,6 +31,38 @@ class TestMisc(unittest.TestCase):
         md_doc.add_section()
 
         md_doc.add_text("The end.\n")
+
+        self.assertEqual(expected_output_text, md_doc.get_markdown_text())
+
+    def test_strong_and_emphasis(self):
+        """Tests of combination of attributes."""
+
+        expected_output_text = get_expected_markdown_text("Strong and em together.text")
+
+        md_doc = doc.Document()
+
+        text = md.gen_bold("This is strong and em.")
+        text = md.gen_italic(text)
+        md_doc.add_text(text)
+        md_doc.add_section()
+
+        md_doc.add_text("So is ")
+        text = md.gen_bold("this")
+        text = md.gen_italic(text)
+        md_doc.add_text(text)
+        md_doc.add_text(" word.")
+        md_doc.add_section()
+
+        text = md.gen_bold("This is strong and em.", True)
+        text = md.gen_italic(text, True)
+        md_doc.add_text(text)
+        md_doc.add_section()
+
+        md_doc.add_text("So is ")
+        text = md.gen_bold("this", True)
+        text = md.gen_italic(text, True)
+        md_doc.add_text(text)
+        md_doc.add_text(" word.\n")
 
         self.assertEqual(expected_output_text, md_doc.get_markdown_text())
 
